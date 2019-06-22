a45b77c42b742431ade1f002c2467baa7acd3274
hpraveen <hariharanster@gmail.com>
2019-06-22 17:33:59 -0400

    created html for 4 crystals and is clickable

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # unit-4-game
+
+//4 crystal and random result
+
+
+//Every crystal has a random number between 1 and 20
+
+
+
+//Either after a win or loss a new random number needs to be generated
+
+
+
+//When a crystal is clicked it needs to add the previous result until it equals the random number
+
+
+
+//For a loss, sum of all clicks should be greater than random number (loss counter incremented by 1)
+
+
+
+//For a win, sum of all clicks should be equal to the random number (win counter incremented by 1)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,39 +3,22 @@ var random_result;
 var lost;
 var win;
 
-//4 crystal and random result
-random_result=Math.floor((Math.random() * 69) + 30;
+
+random_result=Math.floor((Math.random() * 60) + 30);
 $("#result").html('Random Result: '+ random_result);
 
 for (var i=0;i<4;i++){
 
-    var random=Math.floor((Math.random() * 11) + 1);
-    console.log(random);
+    var randomNumber=Math.floor((Math.random() * 11) + 1);
+    console.log(randomNumber);
 
     var crystal=$("<div>");
-    crystal.attr("class",'crystal');
+    crystal.attr({"class":'crystal',"data-random": randomNumber});
 
     $(".crystals").append(crystal);
 }
 
 $(".crystal").on('click',function(){
 
-    console.log($(this));
+    console.log($(this).attr('data-random'));
 })
-//Every crystal has a random number between 1 and 20
-
-
-
-//Either after a win or loss a new random number needs to be generated
-
-
-
-//When a crystal is clicked it needs to add the previous result until it equals the random number
-
-
-
-//For a loss, sum of all clicks should be greater than random number (loss counter incremented by 1)
-
-
-
-//For a win, sum of all clicks should be equal to the random number (win counter incremented by 1)
