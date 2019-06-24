8cb54b2639ead5a869bfec9b10110d76b1beb14d
hpraveen <hariharanster@gmail.com>
2019-06-24 16:50:38 -0400

    Added crystal image and worked on presentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,10 @@ var prev=0;
 
 var resetAndStart=function () {
     $(".crystals").empty();
+    var images =['https://image.shutterstock.com/image-photo/amazing-colorful-quartz-rainbow-flame-600w-736064353.jpg',
+    'https://image.shutterstock.com/image-photo/amethyst-cluster-mineral-specimen-closeup-600w-1412059817.jpg',
+    'https://image.shutterstock.com/image-photo/amazing-colorful-blue-agate-crystal-600w-1012382911.jpg',
+    'https://image.shutterstock.com/image-illustration/3d-render-emerald-green-crystal-600w-674585770.jpg'];
     random_result=Math.floor((Math.random() * 60) + 30);
 $("#result").html('Random Result: '+ random_result);
 
@@ -17,7 +21,8 @@ for (var i=0;i<4;i++){
 
     var crystal=$("<div>");
     crystal.attr({"class":'crystal',"data-random": randomNumber});
-
+    crystal.css({"background-image":"url('"+ images[i] +"')",
+    "background-size":"cover"});
     crystal.html(randomNumber);
 
     $(".crystals").append(crystal);
